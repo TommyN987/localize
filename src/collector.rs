8bc5d2d794f329e929ca9829991131baf843ad4b
tommy <tomas.nagy.tn@gmail.com>
2023-11-23 18:21:05 +0100

    rename analyze functions in Collector to parse

--- src/collector.rs
+++ src/collector.rs
@@ -14,11 +14,11 @@ pub struct Collector {
 
 impl Visit for Collector {
     fn visit_jsx_element(&mut self, jsx_element: &JSXElement) {
-        self.analyze_jsx_element(jsx_element);
+        self.parse_jsx_element(jsx_element);
     }
 
     fn visit_jsx_fragment(&mut self, jsx_fragment: &JSXFragment) {
-        self.analyze_jsx_fragment(jsx_fragment);
+        self.parse_jsx_fragment(jsx_fragment);
     }
 }
 
@@ -53,19 +53,19 @@ impl Collector {
             .collect();
     }
 
-    pub fn analyze_jsx_element(&mut self, jsx_element: &JSXElement) {
+    pub fn parse_jsx_element(&mut self, jsx_element: &JSXElement) {
         for child in &jsx_element.children {
-            self.analyze_jsx_child(child);
+            self.parse_jsx_child(child);
         }
     }
 
-    pub fn analyze_jsx_fragment(&mut self, jsx_fragment: &JSXFragment) {
+    pub fn parse_jsx_fragment(&mut self, jsx_fragment: &JSXFragment) {
         for child in &jsx_fragment.children {
-            self.analyze_jsx_child(child);
+            self.parse_jsx_child(child);
         }
     }
 
-    fn analyze_jsx_child(&mut self, jsx_child: &JSXElementChild) {
+    fn parse_jsx_child(&mut self, jsx_child: &JSXElementChild) {
         match jsx_child {
             JSXElementChild::JSXText(jsx_text) => {
                 if !jsx_text.value.trim().is_empty() {
@@ -73,18 +73,18 @@ impl Collector {
                 }
             }
             JSXElementChild::JSXExprContainer(jsx_expr_container) => {
-                self.analyze_jsx_expression_container(jsx_expr_container)
+                self.parse_jsx_expression_container(jsx_expr_container)
             }
-            JSXElementChild::JSXElement(jsx_element) => self.analyze_jsx_element(jsx_element),
-            JSXElementChild::JSXFragment(jsx_fragment) => self.analyze_jsx_fragment(jsx_fragment),
+            JSXElementChild::JSXElement(jsx_element) => self.parse_jsx_element(jsx_element),
+            JSXElementChild::JSXFragment(jsx_fragment) => self.parse_jsx_fragment(jsx_fragment),
             JSXElementChild::JSXSpreadChild(_) => {}
         }
     }
 
-    fn analyze_jsx_expression_container(&mut self, jsx_expr_container: &JSXExprContainer) {
+    fn parse_jsx_expression_container(&mut self, jsx_expr_container: &JSXExprContainer) {
         match &jsx_expr_container.expr {
             JSXExpr::Expr(expr) => match expr.as_ref() {
-                Expr::Lit(lit) => self.analyze_literal(lit),
+                Expr::Lit(lit) => self.parse_literal(lit),
                 Expr::Ident(ident) => self.variables.push(ident.clone()),
                 Expr::Member(member_expr) => self.object_properties.push(member_expr.clone()),
                 _ => {}
@@ -93,7 +93,7 @@ impl Collector {
         }
     }
 
-    fn analyze_literal(&mut self, lit: &Lit) {
+    fn parse_literal(&mut self, lit: &Lit) {
         match lit {
             Lit::Str(string_literal) => {
                 if !string_literal.value.trim().is_empty() {
@@ -116,73 +116,73 @@ mod test {
     use swc_ecma_ast::{Lit, Str};
 
     #[test]
-    fn analyze_literal_with_non_empty_string() {
-        let mut analyzer = Collector::new();
+    fn parse_literal_with_non_empty_string() {
+        let mut collector = Collector::new();
         let lit = Str {
             value: "Hello World".into(),
             span: swc_common::Span::default(),
             raw: None,
         };
-        analyzer.analyze_literal(&Lit::Str(lit.clone()));
-        assert_eq!(analyzer.string_literals.len(), 1);
-        assert_eq!(analyzer.string_literals[0], lit);
+        collector.parse_literal(&Lit::Str(lit.clone()));
+        assert_eq!(collector.string_literals.len(), 1);
+        assert_eq!(collector.string_literals[0], lit);
     }
 
     #[test]
-    fn analyze_literal_with_empty_string() {
-        let mut analyzer = Collector::new();
+    fn parse_literal_with_empty_string() {
+        let mut collector = Collector::new();
         let lit = Str {
             value: "".into(),
             span: swc_common::Span::default(),
             raw: None,
         };
-        analyzer.analyze_literal(&Lit::Str(lit.clone()));
-        assert!(analyzer.string_literals.is_empty());
+        collector.parse_literal(&Lit::Str(lit.clone()));
+        assert!(collector.string_literals.is_empty());
     }
 
     #[test]
-    fn analyze_literal_with_whitespace_string() {
-        let mut analyzer = Collector::new();
+    fn parse_literal_with_whitespace_string() {
+        let mut collector = Collector::new();
         let lit = Str {
             value: " ".into(),
             span: swc_common::Span::default(),
             raw: None,
         };
-        analyzer.analyze_literal(&Lit::Str(lit.clone()));
-        assert!(analyzer.string_literals.is_empty());
+        collector.parse_literal(&Lit::Str(lit.clone()));
+        assert!(collector.string_literals.is_empty());
     }
     #[test]
-    fn analyze_literal_with_non_empty_jsx_text() {
-        let mut analyzer = Collector::new();
+    fn parse_literal_with_non_empty_jsx_text() {
+        let mut collector = Collector::new();
         let jsx_text = JSXText {
             value: "Some JSX Text".into(),
             span: swc_common::Span::default(),
             raw: swc_atoms::Atom::from("Some JSX Text"),
         };
-        analyzer.analyze_literal(&Lit::JSXText(jsx_text.clone()));
-        assert_eq!(analyzer.jsx_texts.len(), 1);
-        assert_eq!(analyzer.jsx_texts[0], jsx_text);
+        collector.parse_literal(&Lit::JSXText(jsx_text.clone()));
+        assert_eq!(collector.jsx_texts.len(), 1);
+        assert_eq!(collector.jsx_texts[0], jsx_text);
     }
 
     #[test]
-    fn analyze_literal_with_empty_jsx_text() {
-        let mut analyzer = Collector::new();
-        analyzer.analyze_literal(&Lit::JSXText(JSXText {
+    fn parse_literal_with_empty_jsx_text() {
+        let mut collector = Collector::new();
+        collector.parse_literal(&Lit::JSXText(JSXText {
             value: "".into(),
             span: swc_common::Span::default(),
             raw: swc_atoms::Atom::from(""),
         }));
-        assert!(analyzer.jsx_texts.is_empty());
+        assert!(collector.jsx_texts.is_empty());
     }
 
     #[test]
-    fn analyze_literal_with_whitespace_jsx_text() {
-        let mut analyzer = Collector::new();
-        analyzer.analyze_literal(&Lit::JSXText(JSXText {
+    fn parse_literal_with_whitespace_jsx_text() {
+        let mut collector = Collector::new();
+        collector.parse_literal(&Lit::JSXText(JSXText {
             value: "   ".into(),
             span: swc_common::Span::default(),
             raw: swc_atoms::Atom::from("   "),
         }));
-        assert!(analyzer.jsx_texts.is_empty());
+        assert!(collector.jsx_texts.is_empty());
     }
 }
